3b61ae3c7fe80f46486facc2e823460ec3b598ce
Marc Katz <marc.f.katz@gmail.com>
2023-05-11 15:18:51 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function introduction (name, language) {
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+} 
+
+function introductionWithLanguage(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   } 
 
