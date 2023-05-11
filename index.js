d4854a15ba8a491cb4de1085cd9ef18729d30049
Marc Katz <marc.f.katz@gmail.com>
2023-05-11 15:12:53 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function sayHelloTo(name = "User", language = "JavaScript") {
-    console.log(`Hello, ${name}!`);
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
   }
